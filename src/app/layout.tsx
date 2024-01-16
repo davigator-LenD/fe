19d974b258046b9fd85e63d9024208ef558b3545
Danpa_cho <danpa725@cau.ac.kr>
2024-01-16 18:43:54 +0900

    style: update code formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,30 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
 import type { Viewport } from 'next'
+import { Inter } from 'next/font/google'
+import '../style/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 export const viewport: Viewport = {
-  themeColor: '#ffc211',
+    themeColor: '#ffc211',
 }
 export const metadata: Metadata = {
-  title: 'LenD',
-  description: '',
-  icons: {
-    other: [
-      {
-        url: '/icons/splashscreens/iphone5_splash.png',
-        media:
-          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
-        rel: 'apple-touch-startup-image',
-      },
-    ],
-  },
+    title: 'LenD',
+    description: '',
+    icons: {
+        other: [
+            {
+                url: '/icons/splashscreens/iphone5_splash.png',
+                media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+                rel: 'apple-touch-startup-image',
+            },
+        ],
+    },
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en" className="">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }

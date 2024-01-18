008c0ab7e98ec5efe41b60b494660089772073c4
Danpa_cho <danpa725@cau.ac.kr>
2024-01-18 20:58:55 +0900

    feat: add tts api route

---
+++ src/app/api/tts/route.ts
@@ -0,0 +1,36 @@
+import { tts } from '@/interface/tts'
+import { Assistant } from '@/service/assistant'
+import { openAi } from '@/service/open_ai'
+
+const assistant = Assistant.create(openAi)
+
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+    const { text } = tts.body.parse(await req.json())
+
+    try {
+        const ttsBuffer: ArrayBuffer = await assistant.getTTS({
+            input: text,
+        })
+
+        return new Response(ttsBuffer, {
+            status: 200,
+            statusText: 'OK',
+            headers: {
+                'Content-Type': 'audio/mpeg',
+            },
+        })
+    } catch (e) {
+        if (e instanceof Error) {
+            return new Response(null, {
+                status: 500,
+                statusText: 'TTS Error',
+            })
+        }
+        return new Response(null, {
+            status: 500,
+            statusText: 'TTS Error',
+        })
+    }
+}

9671670818c99375150c72c84793cc774b27859d
Danpa_cho <danpa725@cau.ac.kr>
2024-01-18 20:54:23 +0900

    remove: bulk test file

--- src/helpers/__tests__/hi.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-const hi = (name: string) => `Hi ${name}!`
-
-describe('Hi module', () => {
-    it("should return 'Hi John!'", () => {
-        expect(hi('John')).toBe('Hi John!')
-    })
-})

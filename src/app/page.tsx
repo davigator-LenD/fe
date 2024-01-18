79e9d659c5783f16a5791614fb1e7685d58a6955
causyj <jainayj804998@gmail.com>
2024-01-19 01:45:41 +0900

    feat: add Keyboard svg component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import SvgVoice from '@/lib/icons/Voice'
+import SvgKeyboard from '@/lib/icons/Keyboard'
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <SvgVoice />
+            <SvgKeyboard />
         </main>
     )
 }
---
+++ src/lib/icons/Keyboard.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+const SvgKeyboard = (props: SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="85" height="85" fill="none" {...props}>
+        <g filter="url(#a)">
+            <rect width="85" height="85" fill="#959595" fill-opacity=".2" rx="42.5" />
+        </g>
+        <path stroke="#fff" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M47.43 45.84H36.58" />
+        <path
+            stroke="#fff"
+            stroke-miterlimit="10"
+            stroke-width="2"
+            d="M53.7 33H30.3a2.3 2.3 0 0 0-2.3 2.3v13.96a2.3 2.3 0 0 0 2.3 2.3h23.4a2.3 2.3 0 0 0 2.3-2.3V35.3a2.3 2.3 0 0 0-2.3-2.3Z"
+        />
+        <path
+            fill="#fff"
+            d="M42 39.58a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84ZM37 39.58a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84ZM32 39.58a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84ZM47 39.58a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84ZM52 39.58a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84ZM32 47.26a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84ZM52 47.26a1.42 1.42 0 1 0 0-2.84 1.42 1.42 0 0 0 0 2.84Z"
+        />
+        <defs>
+            <filter
+                id="a"
+                width="365"
+                height="365"
+                x="-140"
+                y="-140"
+                color-interpolation-filters="sRGB"
+                filterUnits="userSpaceOnUse"
+            >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feGaussianBlur in="BackgroundImageFix" stdDeviation="70" />
+                <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_97_119" />
+                <feBlend in="SourceGraphic" in2="effect1_backgroundBlur_97_119" result="shape" />
+            </filter>
+        </defs>
+    </svg>
+)
+export default SvgKeyboard

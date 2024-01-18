547214ca79ecc40b34c8aa1d2f35fe108313f4ad
causyj <jainayj804998@gmail.com>
2024-01-19 01:37:59 +0900

    feat: add Note svg component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Back from '@/lib/icons/Back'
+import SvgNote from '@/lib/icons/Note'
 import SvgSend from '@/lib/icons/Send'
 import WebList from '@/lib/icons/WebList'
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <SvgSend />
+            <SvgNote />
         </main>
     )
 }

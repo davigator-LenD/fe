c2b5702c44939314059a142199d0b1429f987a8a
causyj <jainayj804998@gmail.com>
2024-01-19 01:27:29 +0900

    feat: add WebList svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import WebList from '@/lib/icons/WebList'
 export default function Home() {
-    return <main className="flex min-h-screen flex-col items-center justify-between p-24">hi</main>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <WebList />
+        </main>
+    )
 }

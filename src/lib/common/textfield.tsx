2c595b88ccd864f1717d02c27a5cd4a5b0c512b3
causyj <jainayj804998@gmail.com>
2024-01-20 02:09:11 +0900

    feat: add send button

--- src/lib/common/textfield.tsx
+++ src/lib/common/textfield.tsx
@@ -1,9 +1,10 @@
 'use client'
 import React, { FormEvent, useState } from 'react'
 import { useEffect } from 'react'
+import SvgSend from '../icons/send'
 
 export const TextField = () => {
-    const [question, setQuestion] = useState('')
+    const [website, setWebsite] = useState('')
     const [inputHeight, setInputHeight] = useState(60)
     useEffect(() => {
         const handleResize = () => {
@@ -18,22 +19,29 @@ export const TextField = () => {
     }, [])
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        alert(`question: ${question}`)
+        alert(`website: ${website}`)
     }
 
     return (
         <div className="flex h-screen w-full items-center justify-center p-8">
-            <div className={` fixed bottom-0 p-[10px]`} style={{ height: inputHeight }}>
-                <form className=" flex w-full opacity-20" onSubmit={handleSubmit}>
+            <div className={`fixed bottom-4 flex w-full  items-center p-[10px]`} style={{ height: inputHeight }}>
+                <form className=" flex w-full " onSubmit={handleSubmit}>
                     <input
-                        className="flex w-full rounded-3xl border-2 bg-transparent p-2 px-6 shadow-inner placeholder:text-theme-font"
-                        id="name"
+                        className={`flex max-h-[80px] w-full items-center rounded-3xl border-2 border-primary-box bg-transparent p-3 px-6 caret-primary-logo shadow-inner ${website === '' ? 'placeholder:text-primary-box' : ' placeholder:text-theme-font'}`}
+                        id="textfield1"
                         type="text"
-                        value={question}
-                        onChange={(e) => setQuestion(e.target.value)}
+                        value={website}
+                        onChange={(e) => setWebsite(e.target.value)}
                         placeholder="웹 사이트 이름"
                     />
                 </form>
+
+                {/* {website === '' ? <div></div> 
+               :
+               <div className="z-10 ml-[-40px] flex items-center justify-end"
+               >
+               <SvgSend />
+               </div> } */}
             </div>
         </div>
     )

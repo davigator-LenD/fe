17eb380bb810443bf3f80acb3ca7163981d56d28
causyj <jainayj804998@gmail.com>
2024-01-20 00:31:46 +0900

    feat: add common font style and color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,28 @@ const config: Config = {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
             },
+            fontFamily: {
+                eng: ['var(--bylander)'],
+                kor: ['var(--pretendard)'],
+            },
+            fontSize: {
+                '8xl': '6rem',
+                '6xl': '3.75rem',
+                '4xl': '2.5rem', //bold 40px
+                '3xl': '1.5rem', //bold 24px
+                '2xl': '1.25rem', //regular : 20px
+                xl: '0.875rem', //medium : 14px
+            },
+            colors: {
+                background: '#121212',
+                font: '#FFFFFF',
+                primary: {
+                    logo: '#EBFE83',
+                    icon: '#959595',
+                    box: '#262626',
+                    toggleBG: '#171413',
+                },
+            },
         },
     },
     plugins: [require('tailwindcss-3d'), require('tailwind-scrollbar-hide')],

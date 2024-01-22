0baa2675153db9e9ad484006471f449bee144202
causyj <jainayj804998@gmail.com>
2024-01-22 22:56:09 +0900

    feat: add iframe of gov.site

--- src/app/web_view/page.tsx
+++ src/app/web_view/page.tsx
@@ -1,3 +1,15 @@
 export default function WebViewPage() {
-    return <div>Web view page</div>
+    return (
+        <div className="size-full">
+            <iframe
+                src={`https://www.gov.kr/yearend_main.html`}
+                //         //src={`https://www.youtube.com/embed/${videoId}`}
+                //         //src={`https://shopping.naver.com/`}
+                //         //쿠팡 & 국민은행 & 정부24 & 정부정책 &
+                //         src="https://www.google.com/webhp?igu=1"
+                height="100%"
+                allowFullScreen
+            />
+        </div>
+    )
 }

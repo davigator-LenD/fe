3d18a8e239e764ff072a59e91673d8e784c69143
causyj <jainayj804998@gmail.com>
2024-01-24 12:08:55 +0900

    feat: tidy up icons&text

--- src/app/web_list/page.tsx
+++ src/app/web_list/page.tsx
@@ -1,3 +1,18 @@
+import { Text } from '@/lib/common'
+import { SvgBack } from '@/lib/icons'
+import { SvgWebListVoice } from '@/lib/icons/weblist_voice'
+
 export default function WebListPage() {
-    return <div>Web list page</div>
+    return (
+        <div className="flex size-full flex-col items-start justify-start">
+            <div className="mt-8 flex w-full flex-row items-center justify-between">
+                <SvgBack />
+                <SvgWebListVoice />
+            </div>
+            <div className="mx-8 size-full ">
+                <Text.Heading>웹 사이트</Text.Heading>
+                <Text.SubHeading>어디 웹 사이트에 들어갈까요?</Text.SubHeading>
+            </div>
+        </div>
+    )
 }

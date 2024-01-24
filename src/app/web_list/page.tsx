390ead64f4e7e7254b82a4da1fe33bf2f52f1dce
causyj <jainayj804998@gmail.com>
2024-01-24 12:22:17 +0900

    feat: update textfield

--- src/app/web_list/page.tsx
+++ src/app/web_list/page.tsx
@@ -1,17 +1,36 @@
+'use client'
+import { useState } from 'react'
 import { Text } from '@/lib/common'
-import { SvgBack } from '@/lib/icons'
+import { SvgBack, SvgWebList } from '@/lib/icons'
 import { SvgWebListVoice } from '@/lib/icons/weblist_voice'
 
 export default function WebListPage() {
+    const [question, setQuestion] = useState('')
     return (
         <div className="flex size-full flex-col items-start justify-start">
             <div className="mt-8 flex w-full flex-row items-center justify-between">
                 <SvgBack />
                 <SvgWebListVoice />
             </div>
-            <div className="mx-8 size-full ">
+            <div className="flex size-full flex-col px-5 ">
                 <Text.Heading>웹 사이트</Text.Heading>
                 <Text.SubHeading>어디 웹 사이트에 들어갈까요?</Text.SubHeading>
+                <div className="mt-4 flex w-full flex-row items-center justify-center">
+                    <form className={` flex w-full `}>
+                        <input
+                            className={`flex max-h-[80px] w-full items-center  border-[1px] border-background border-b-white bg-background p-3  caret-primary-logo shadow-inner placeholder:text-3xl ${question === '' ? 'placeholder:text-primary-box' : ' placeholder:text-theme-font'}`}
+                            id="textfield1"
+                            type="text"
+                            value={question}
+                            onChange={(e) => setQuestion(e.target.value)}
+                            placeholder="웹 사이트 입력"
+                        />
+                    </form>
+
+                    <div className={`  ml-[-32px] flex items-center justify-end`}>
+                        <SvgWebList />
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/lib/common/heading.tsx
+++ src/lib/common/heading.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export const Text = ({ children }: React.PropsWithChildren) => <div className="flex flex-col">{children}</div>
-export const Heading = ({ children }: React.PropsWithChildren) => <h1 className="text-4xl font-[700]">{children}</h1>
-const SubHeading = ({ children }: React.PropsWithChildren) => <h3 className="text-2xl font-[500]">{children}</h3>
+export const Heading = ({ children }: React.PropsWithChildren) => <h1 className="text-4xl font-[600]">{children}</h1>
+const SubHeading = ({ children }: React.PropsWithChildren) => <h3 className="text-2xl font-[400]">{children}</h3>
 Text.Heading = Heading
 Text.SubHeading = SubHeading

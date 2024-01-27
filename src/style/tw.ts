94222851037ffad43c7af220e02611041a49298a
Danpa_cho <danpa725@cau.ac.kr>
2024-01-27 19:23:45 +0900

    feat: adjust tailwind config at utils

--- src/style/tw.ts
+++ src/style/tw.ts
@@ -1,4 +1,18 @@
 import { createTools, type Tailwindest } from 'tailwindest'
 
-export const tw = createTools<Tailwindest>()
-export type Tw = Tailwindest
+export const tw = createTools<Tw>()
+export type Tw = Tailwindest<
+    {
+        color:
+            | 'primary-logo'
+            | 'primary-icon'
+            | 'primary-box'
+            | 'primary-toggleBG'
+            | 'primary-textfieldBOX'
+            | 'background'
+    },
+    {
+        fontFamily: 'eng' | 'kor'
+        fontSize: '8xl' | '6xl' | '4xl' | '3xl' | '2xl' | 'xl'
+    }
+>

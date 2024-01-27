11559a1e696792a5d57efbb3c55e7d12d4d2e82c
Danpa_cho <danpa725@cau.ac.kr>
2024-01-27 19:24:05 +0900

    feat: add voice small icon

---
+++ src/lib/icons/voice_small.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import type { SVGProps } from 'react'
+export const SvgVoiceSmall = (props: SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={11} height={12} fill="none" {...props}>
+        <path
+            stroke="#EBFE83"
+            strokeLinecap="round"
+            strokeMiterlimit={10}
+            strokeWidth={1.046}
+            d="M5.463 3.134v5.601M.902 4.614v2.642M3.182 1v9.87M7.743 5.038v1.794M10.023 6.261v-.36"
+        />
+    </svg>
+)
